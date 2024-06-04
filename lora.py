1eb33ce96ed8f449d05c78e7614e61e854b12261
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-06-04 16:09:30 +1000

    Add files via upload

--- lora.py
+++ lora.py
@@ -3,8 +3,11 @@
 #pip install llama-index-graph-stores-nebula
 #pip install llama-index-llms-azure-openai
 
+
 import browserbase
 import llama_index
+from llama_index.core import Settings
+from llama_index.llms.openai import OpenAI
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core.node_parser import SentenceSplitter
 chunk_data = SentenceSplitter(chunk_size =1024)
@@ -17,7 +20,7 @@ nodes = chunk_data.get_nodes_from_documents(datasets)
 node_metadata = nodes[0].get_content(metadata_mode = True)
 print(str(node_metadata))
 
-len(nodes)
+print(len(nodes))
 #print(datasets)
 
 
