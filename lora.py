1f43a643ecaa15cb8f11507b7886f2a42f03bda9
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-06-04 15:45:57 +1000

    Add files via upload

--- lora.py
+++ lora.py
@@ -6,6 +6,7 @@
 import browserbase
 import llama_index
 from llama_index.core import SimpleDirectoryReader
+from llama_index.core.node_parser import SentenceSplitter
 
 datasets = SimpleDirectoryReader(input_files = ["/home/saul/Desktop/agentic_rag/basics/lora.pdf"]).load_data()
 
