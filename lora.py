fe725557c2b5f7496d50413b5eca52838f41acc3
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-06-04 14:31:46 +1000

    Add files via upload

---
+++ lora.py
@@ -0,0 +1,8 @@
+#pip install llama-index-llms-openai
+#pip install llama-index-embeddings-openai
+#pip install llama-index-graph-stores-nebula
+#pip install llama-index-llms-azure-openai
+
+import llama_index.core 
+
+
