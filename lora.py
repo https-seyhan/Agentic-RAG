a641193e373af879c113f6b416e11a6464fb54c7
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-06-04 15:33:19 +1000

    Add files via upload

--- lora.py
+++ lora.py
@@ -3,8 +3,14 @@
 #pip install llama-index-graph-stores-nebula
 #pip install llama-index-llms-azure-openai
 
+import browserbase
+import llama_index
 from llama_index.core import SimpleDirectoryReader
 
-datasets = SimpleDirectoryReader(input_files = ["lora.pdf"])
+datasets = SimpleDirectoryReader(input_files = ["/home/saul/Desktop/agentic_rag/basics/lora.pdf"]).load_data()
+
+
+
+
 
 
