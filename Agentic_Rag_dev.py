a9e605135f8faa948538316e15abc4a37ab2bd5b
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-06-23 22:09:55 +1000

    Add files via upload

---
+++ Agentic_Rag_dev.py
@@ -0,0 +1,9 @@
+
+#  Document Corpus
+document_corpus = [
+    "The Eiffel Tower is located in Paris, France.",
+    "The Great Wall of China is a historic fortification.",
+    "The Amazon Rainforest is known for its biodiversity.",
+    "Mount Everest is the highest mountain in the world.",
+    "The Nile River is the longest river in the world."
+]
