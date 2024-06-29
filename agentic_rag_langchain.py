71531fe117e1be4613cce400b3897207414f927c
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-06-29 12:00:07 +1000

    Add files via upload

--- agentic_rag_langchain.py
+++ agentic_rag_langchain.py
@@ -5,12 +5,16 @@ import nest_asyncio
 from llama_index.llms.openai import OpenAI
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core import Settings
+from llama_index.core import SimpleDirectoryReader
 
 #from dotenv import load_dotenv
 # This is needed for jupyter notebook to do asynchronous rendering
 nest_asyncio.apply()
 # Ensure you replace this before executing this cell
 #load_dotenv("<path to your environment file where OpenAI API key is stored as OPENAI_API_KEY=sk-#######>.env")
+
+data_dir = '/home/saul/Desktop/agentic_rag'
+data_dir = '/home/saul/Desktop/agentic_rag'
 
 # Setup OpenAI Model and Embeddings used for indexing the documents
 Settings.llm = OpenAI(model='gpt-4-0125-preview', temperature=0.2)
@@ -28,15 +32,12 @@ def get_index(index_name, doc_file_path):
     index = VectorStoreIndex.from_documents(documents)
     # Store the index to disk
     index.storage_context.persist(f"{PERSIST_INDEX_DIR}{index_name}/")
-  else: # Load index from disk
-    storage_context = StorageContext.from_defaults(persist_dir=f"{PERSIST_INDEX_DIR}{index_name}/")
-    index = load_index_from_storage(storage_context)
 
   return index
 
 
 # Setup Uber and Lyft Vector Indices
-uber_index = get_index("uber_10k",f"/{data_dir}/RAG/data/10k/uber_10k_2023.pdf")
-lyft_index = get_index("lyft_10k",f"/{data_dir}/RAG/data/10k/lyft_10k_2023.pdf")
-expedia_index = get_index("expedia_10k", f"/{data_dir}/RAG/data/10k/expedia_10k_2023.pdf")
-booking_index = get_index("booking_10k", f"/{data_dir}/RAG/data/10k/booking_10k_2023.pdf")
+uber_index = get_index("uber_10k",f"/{data_dir}/uber_10k_2023.pdf")
+lyft_index = get_index("lyft_10k",f"/{data_dir}/lyft_10k_2023.pdf")
+expedia_index = get_index("expedia_10k", f"/{data_dir}/expedia_10k_2023.pdf")
+booking_index = get_index("booking_10k", f"/{data_dir}/booking_10k_2023.pdf")
